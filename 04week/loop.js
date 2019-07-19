336117a8a7ed41f69bb3f6c1a49050c7f66615eb
kpatt713 <kpatt713@gmail.com>
2019-07-18 19:07:59 -0500

    unfinished loops project

---
+++ 04week/loop.js
@@ -0,0 +1,27 @@
+'use strict';
+//create do...while loop to console log numbers 1-1000
+let i = 0
+do {
+i += 1;
+console.log(i);
+} while (i<1000);
+
+//2.
+
+let person = {
+firstName: "Jane"
+lastName: "Doe"
+birthDate: "Jan 5, 1925"
+gender: "female"
+};
+
+//3. 
+
+ let birthYear = person.birthDate[person.birthDate.length -1];
+    
+if(birthYear % 2 !==0) {
+console.log(person.birthDate)
+}
+
+//4. 
+
